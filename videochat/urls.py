0565ffe221fece17d617a44cb6977f106af31ab8
Aaditya Agrawal <Aaditya.Agrawal@cuelogic.com>
2018-02-16 12:22:43 +0530

    basic project setup

--- videochat/urls.py
+++ videochat/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,9 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('videochatapp.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    # url(r'^accounts/', include('django.contrib.auth.urls')),
+
 ]
