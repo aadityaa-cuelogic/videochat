4abbbe08e746434b1b9abffb7f3fd8cf0c4666e9
rizwan <rizwan.shaikh@cuelogic.com>
2018-02-19 19:03:13 +0530

    change request.POST to request.POST.get

--- videochat/views.py
+++ videochat/views.py
@@ -8,24 +8,17 @@ def register(request):
 
     if request.method == 'POST':
         user = User.objects.filter(username =request.POST['username'])
-
         if user:
             messages.error(request,'User Already Exist.')
             return HttpResponseRedirect('/register',{"messages":messages})
-
-        password = make_password(request.POST['password'])
-
-        user  = User(username=request.POST['username'],
-                    email = request.POST['email'],
+        password = make_password(request.POST.get('password'))
+        user  = User(username=request.POST.get('username'),
+                    email = request.POST.get('email'),
                     password = password,
-                    first_name = request.POST['firstname'],
-                    last_name = request.POST['lastname']
+                    first_name = request.POST.get('firstname'),
+                    last_name = request.POST.get('lastname')
                 )
-
         user.save()
-
         messages.success(request, 'User Register Successfully.')
-        
         return HttpResponseRedirect('/login',{"messages":messages})
-
     return render(request,'registration/register.html')
