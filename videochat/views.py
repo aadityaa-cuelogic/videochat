f37eebba1e3443f016e2cc69123ae3ccd4a99613
rizwan <rizwan.shaikh@cuelogic.com>
2018-02-19 19:16:58 +0530

    register function updated for code standand

--- videochat/views.py
+++ videochat/views.py
@@ -5,27 +5,19 @@ from django.http import HttpResponseRedirect
 
 
 def register(request):
-
     if request.method == 'POST':
-        user = User.objects.filter(username =request.POST['username'])
-
+        user = User.objects.filter(username=request.POST.get('username'))
         if user:
             messages.error(request,'User Already Exist.')
             return HttpResponseRedirect('/register',{"messages":messages})
-
-        password = make_password(request.POST['password'])
-
-        user  = User(username=request.POST['username'],
-                    email = request.POST['email'],
-                    password = password,
-                    first_name = request.POST['firstname'],
-                    last_name = request.POST['lastname']
+        password = make_password(request.POST.get('password'))
+        user  = User(username=request.POST.get('username'),
+                    email=request.POST.get('email'),
+                    password=password,
+                    first_name=request.POST.get('firstname'),
+                    last_name=request.POST.get('lastname')
                 )
-
         user.save()
-
         messages.success(request, 'User Register Successfully.')
-        
         return HttpResponseRedirect('/login',{"messages":messages})
-
     return render(request,'registration/register.html')
