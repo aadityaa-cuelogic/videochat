05bacb84964cf793d3c816c01533c9b52b759105
Aaditya Agrawal <Aaditya.Agrawal@cuelogic.com>
2018-02-20 16:03:46 +0530

    base line for create conf

--- videochatapp/urls.py
+++ videochatapp/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.home, name='home'),
-	# url(r'videochat', views.videochat, name='videochat' ),
-	url(r'^videochat/(?P<roomkey>[a-zA-Z0-9]+)/$',views.videochat,name="videochat"),
-	url(r'^create_conference/$',views.create_conference,name="create_conference")
+
+    url(r'^$', views.home, name='home'),
+    url(r'^videochat/(?P<roomkey>[a-zA-Z0-9]+)/join$',views.join_videochat,name="joinvideochat"),
+    url(r'^videochat/createvideochatroom/$',views.create_videochat_room,name="createvideochatroom")
 
 ]
--- videochatapp/views.py
+++ videochatapp/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
+from .models import ConferenceRoom, ConferenceRoomRoles, ConferenceRoomParticipants
 # Create your views here.
 def home(request):
-	return render(request, 'videochatapp/home.html')
+    """
+    Function to show home page
+    """
+    return render(request, 'videochatapp/home.html')
 
 @login_required(login_url='/login/')
-def videochat(request,roomkey):
-	return render(request, 'videochatapp/videochat.html',{'roomkey':roomkey})
+def join_videochat(request, roomkey):
+    """
+    Function to show join videochat room
+    """
+    return render(request, 'videochatapp/videochat.html',{'roomkey':roomkey})
 
 @login_required(login_url='/login/')
-def create_conference(request):
+def create_videochat_room(request):
+    """
+    Function to create videochat room
+    """
+    if request.method == 'POST':
+        pass
+    else:
+        try:
+            # Get all participants users excluding current user
+            participants = User.objects.all().exclude(id=request.user.id)
+        except:
+            raise
 
-	users = User.objects.all().exclude(id=request.user.id)
-
-	if users:
-		return render(request, 'videochatapp/create_conference.html',{"users":users})	
-
-	
+        # Get all user roles
+        participants_roles = ConferenceRoomRoles.objects.all()
+        context = {
+            'participants': participants,
+            'participants_roles' : participants_roles,
+        }
+        return render(request, 'videochatapp/create_videochat_room.html',context)
